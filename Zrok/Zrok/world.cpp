558208755b23884e069eefd03efbf6f610244f76
SasiKoka <borja.sasieta@gmail.com>
2018-07-12 20:52:31 +0200

    S'afageix la classe Principal del joc
    Anomenada world, aquesta classe sera la que fara el funcionament del joc.
    
    que despres l'usarem a la Classe main.

---
+++ Zrok/Zrok/world.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "comunicate.h"
+#include "world.h"
+
+// ----------------------------------------------------
+World::World()
+{
+	tick_timer = clock();
+}
+
+// ----------------------------------------------------
+World::~World()
+{
+	
+}
+
+// ----------------------------------------------------
+bool World::Tick(vector<string>& args)
+{
+	bool ret = true;
+
+	if (args.size() > 0 && args[0].length() > 0)
+		ret = ParseCommand(args);
+
+	GameLoop();
+
+	return ret;
+}
+
+// ----------------------------------------------------
+void World::GameLoop()
+{
+	clock_t now = clock();
+
+	if ((now - tick_timer) / CLOCKS_PER_SEC > TICK_FREQUENCY)
+	{
+		tick_timer = now;
+	}
+}
+
+// ----------------------------------------------------
+bool World::ParseCommand(vector<string>& args)
+{
+	bool ret = true;
+	return ret;
+}
---
+++ Zrok/Zrok/world.h
@@ -0,0 +1,29 @@
+#ifndef __World__
+#define __World__
+
+#include <string>
+#include <list>
+#include <time.h>
+
+#define TICK_FREQUENCY 0.5f
+
+using namespace std;
+
+class World
+{
+public:
+
+	World();
+	~World();
+
+	bool Tick(vector<string>& args);
+	bool ParseCommand(vector<string>& args);
+	void GameLoop();
+
+private:
+
+	clock_t tick_timer;
+	
+};
+
+#endif //__World__
